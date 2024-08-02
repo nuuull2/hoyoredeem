2096dd7f7c512bac195e62bd859ed4860fed81d3
nuuull <nuuull@courvix.com>
2024-08-02 22:24:19 +0800

    change user agent

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ CODES_URL = {
 
 async def get_codes_upstream(game):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"
     }
 
     async with aiohttp.ClientSession() as session:
